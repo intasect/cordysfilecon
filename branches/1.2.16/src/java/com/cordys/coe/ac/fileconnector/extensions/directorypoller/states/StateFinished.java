7321c72fc557ac163a493fd1e4e9355d67258e53
yravi@cordys.com <yravi@cordys.com@5a65cf53-0225-fabc-b887-f631fb90e4f5>
2010-12-02 05:29:57 +0000

    Added fix for the ReadFileRecords Method.

---
+++ branches/1.2.16/src/java/com/cordys/coe/ac/fileconnector/extensions/directorypoller/states/StateFinished.java
@@ -0,0 +1,85 @@
+
+/**
+ * Copyright 2005 Cordys R&D B.V. 
+ * 
+ * This file is part of the Cordys SAP Connector. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ package com.cordys.coe.ac.fileconnector.extensions.directorypoller.states;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+/**
+ * File state when file processing is finished.
+ *
+ * @author  mpoyhone
+ */
+public class StateFinished
+    implements IFileState
+{
+    /**
+     * @see  com.cordys.coe.ac.fileconnector.extensions.directorypoller.states.IFileState#execute()
+     */
+    public boolean execute()
+                    throws FileStateException
+    {
+        return true;
+    }
+
+    /**
+     * @see  com.cordys.coe.ac.fileconnector.extensions.directorypoller.states.IFileState#readFromLog(DataInputStream,
+     *       boolean, XMLStreamWriter)
+     */
+    public void readFromLog(DataInputStream in, boolean finished, XMLStreamWriter logWriter)
+                     throws IOException, XMLStreamException
+    {
+    }
+
+    /**
+     * @see  com.cordys.coe.ac.fileconnector.extensions.directorypoller.states.IFileState#writeToLog(DataOutputStream)
+     */
+    public void writeToLog(DataOutputStream out)
+                    throws IOException
+    {
+    }
+
+    /**
+     * @see  com.cordys.coe.ac.fileconnector.extensions.directorypoller.states.IFileState#getPreviousState()
+     */
+    public IFileState getPreviousState()
+    {
+        return null;
+    }
+
+    /**
+     * @see  com.cordys.coe.ac.fileconnector.extensions.directorypoller.states.IFileState#getStateType()
+     */
+    public EFileState getStateType()
+    {
+        return EFileState.FINISHED;
+    }
+
+    /**
+     * @see  com.cordys.coe.ac.fileconnector.extensions.directorypoller.states.IFileState#isFinished()
+     */
+    public boolean isFinished()
+    {
+        return true;
+    }
+}
