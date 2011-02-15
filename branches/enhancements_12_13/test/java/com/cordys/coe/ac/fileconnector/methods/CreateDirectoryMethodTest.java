6165126c476041aaec45e9db1f5ccf0cc8d4e72d
phanisrinivas@gmail.com <phanisrinivas@gmail.com@5a65cf53-0225-fabc-b887-f631fb90e4f5>
2011-02-15 17:12:29 +0000

    Test case for CreateDirectory API - Enhancement 13

--- branches/enhancements_12_13/test/java/com/cordys/coe/ac/fileconnector/methods/CreateDirectoryMethodTest.java
+++ branches/enhancements_12_13/test/java/com/cordys/coe/ac/fileconnector/methods/CreateDirectoryMethodTest.java
@@ -25,16 +25,42 @@ public class CreateDirectoryMethodTest extends FileConnectorTestCase
                 "  <parentDirectoryPath>{1}</parentDirectoryPath>\r\n" + 
                 "</CreateDirectory>";
         String outputSoapXml = 
-                "<CreateDirectoryResponse  xmlns=\"http://schemas.cordys.com/1.0/ac/FileConnector\" />"; 
+                "<response  xmlns=\"http://schemas.cordys.com/1.0/ac/FileConnector\" />"; 
         
         String newDirName = "subdir";
         File parentDir = tempFolder;
-        CopyFileMethod method = new CopyFileMethod();
+        CreateDirectoryMethod method = new CreateDirectoryMethod();
         ISoapRequestContext request = executeMethod(method, MessageFormat.format(inputSoapXml, 
         																		 newDirName,
         																		 parentDir.getAbsolutePath()));
         
-        File newAbsDir = GeneralUtils.getAbsoluteFile(newDirName,parentDir);
+        File newAbsDir = GeneralUtils.getAbsoluteFile(newDirName,parentDir);        
+        assertNodesEqual(parse(outputSoapXml), request.getResponseRootNode());
+        assertTrue("New Destination folder does not exist.", newAbsDir.exists());       
+    }
+    
+    /**
+     * Test method for CreateDirectory method.
+     * @throws Exception Thrown
+     */
+    public void testCreateDirectoryWithMultipleDirProcess() throws Exception
+    {
+        String inputSoapXml = 
+                "<CreateDirectory xmlns=\"http://schemas.cordys.com/1.0/ac/FileConnector\">\r\n" + 
+                "  <newDirectoryName>{0}</newDirectoryName>\r\n" + 
+                "  <parentDirectoryPath>{1}</parentDirectoryPath>\r\n" + 
+                "</CreateDirectory>";
+        String outputSoapXml = 
+                "<response  xmlns=\"http://schemas.cordys.com/1.0/ac/FileConnector\" />"; 
+        
+        String newDirName = "subdir1/subdir2";
+        File parentDir = tempFolder;
+        CreateDirectoryMethod method = new CreateDirectoryMethod();
+        ISoapRequestContext request = executeMethod(method, MessageFormat.format(inputSoapXml, 
+        																		 newDirName,
+        																		 parentDir.getAbsolutePath()));
+        
+        File newAbsDir = GeneralUtils.getAbsoluteFile(newDirName,parentDir);        
         assertNodesEqual(parse(outputSoapXml), request.getResponseRootNode());
         assertTrue("New Destination folder does not exist.", newAbsDir.exists());       
     }
