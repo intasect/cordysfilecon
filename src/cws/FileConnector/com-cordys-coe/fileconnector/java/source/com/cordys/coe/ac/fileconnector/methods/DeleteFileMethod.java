3e757daf827e00fc61913f4ff645d9f78d946655
gkaviyarasu <gkaviyarasu@cordys.com>
2012-07-02 10:43:41 +0000

    Migrating the connector to BOP4.1 CU7

---
+++ src/cws/FileConnector/com-cordys-coe/fileconnector/java/source/com/cordys/coe/ac/fileconnector/methods/DeleteFileMethod.java
@@ -0,0 +1,124 @@
+
+/**
+ * Copyright 2005 Cordys R&D B.V. 
+ * 
+ * This file is part of the Cordys File Connector. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ package com.cordys.coe.ac.fileconnector.methods;
+
+import com.cordys.coe.ac.fileconnector.ApplicationConfiguration;
+import com.cordys.coe.ac.fileconnector.IFileConnectorMethod;
+import com.cordys.coe.ac.fileconnector.ISoapRequestContext;
+import com.cordys.coe.ac.fileconnector.LogMessages;
+import com.cordys.coe.ac.fileconnector.exception.ConfigException;
+import com.cordys.coe.ac.fileconnector.exception.FileException;
+import com.cordys.coe.ac.fileconnector.utils.XmlUtils;
+
+import java.io.File;
+
+/**
+ * Implements DeleteFile SOAP method.
+ *
+ * @author  mpoyhone
+ */
+public class DeleteFileMethod
+    implements IFileConnectorMethod
+{
+    /**
+     * Contains the SOAP method name.
+     */
+    public static final String METHOD_NAME = "DeleteFile";
+    /**
+     * Filename request parameter for DeleteFile and methods.
+     */
+    private static final String PARAM_FILENAME = "fileName";
+    /**
+     * Contains the FileConnector configuration.
+     */
+    private ApplicationConfiguration acConfig;
+
+    /**
+     * @see  com.cordys.coe.ac.fileconnector.IFileConnectorMethod#cleanup()
+     */
+    public void cleanup()
+                 throws ConfigException
+    {
+    }
+
+    /**
+     * @see  com.cordys.coe.ac.fileconnector.IFileConnectorMethod#initialize(com.cordys.coe.ac.fileconnector.ApplicationConfiguration)
+     */
+    public boolean initialize(ApplicationConfiguration acConfig)
+                       throws ConfigException
+    {
+        this.acConfig = acConfig;
+
+        return true;
+    }
+
+    /**
+     * @see  com.cordys.coe.ac.fileconnector.IFileConnectorMethod#onReset()
+     */
+    public void onReset()
+    {
+    }
+
+    /**
+     * @see  com.cordys.coe.ac.fileconnector.IFileConnectorMethod#process(com.cordys.coe.ac.fileconnector.ISoapRequestContext)
+     */
+    public EResult process(ISoapRequestContext req)
+                    throws FileException
+    {
+        int requestNode = req.getRequestRootNode();
+
+        // Get the needed parameters from the SOAP request
+        String sFileName = XmlUtils.getStringParameter(requestNode, PARAM_FILENAME, true);
+
+        // Create File objects for the source and destination files
+        File fFile = new File(sFileName);
+
+        // Do some sanity checking.
+        if (!acConfig.isFileAllowed(fFile))
+        {
+            throw new FileException(LogMessages.FILE_ACCESS_NOT_ALLOWED);
+        }
+
+        if (!fFile.exists())
+        {
+            throw new FileException(LogMessages.FILE_NOT_EXIST,sFileName);
+        }
+
+        if (fFile.isDirectory())
+        {
+            throw new FileException(LogMessages.FILE_IS_ADIRECTORY);
+        }
+
+        // Delete the file
+        if (!fFile.delete())
+        {
+            throw new FileException(LogMessages.UNABLE_TO_DELETE_FILE);
+        }
+
+        return EResult.FINISHED;
+    }
+
+    /**
+     * @see  com.cordys.coe.ac.fileconnector.IFileConnectorMethod#getMethodName()
+     */
+    public String getMethodName()
+    {
+        return METHOD_NAME;
+    }
+}
